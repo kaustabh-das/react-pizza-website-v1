a3d34ce7fc88d62bbc8bf104d116e020fa6d54bf
Kaustabh Kumar Das <kaustabhkumar1997@gmail.com>
2020-12-12 20:20:07 +0530

    0.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import  { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
+import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data';
+import Feature from './components/Feature';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle />
+      {/* <Navbar /> I remove the navbar because I put it in the hero section */} 
+      <Hero />
+      <Products heading='Choose your favorite' data={productData} />
+      <Feature />
+      <Products heading='Sweet Treats for You' data={productDataTwo} />
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import {
+  FooterContainer,
+  FooterWrap,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcons,
+  SocialIconLink,
+  SocialPera
+} from './FooterElements';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to='/'>Pizza</SocialLogo>
+            {/* <SocialPera>All Copy Right Reserved.</SocialPera> */}
+            <SocialIcons>
+              <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink
+                href='#'
+                target='_blank'
+                aria-label='Twitter'
+                rel=''
+              >
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href='https://www.linkedin.com/in/kaustabh-das-b626a018b/' 
+                target='_blank' 
+                aria-label='Linkedin'
+                rel='noopener noreferrer'
+              >
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
